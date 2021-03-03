b5c58557ad6631c04eaebedb61b71c6c3d7c9514
OmerBotbol <omerbot2@gmail.com>
2021-03-03 13:32:55 +0200

    adding validation for host name

--- shorturl/shorturl.js
+++ shorturl/shorturl.js
@@ -2,24 +2,30 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const shortid = require("shortid")
+const dns = require("dns");
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }))
 
-router.post("/:new", (req, res)=>{
+router.post("/new", (req, res)=>{
     const givenUrl = req.body.url;
-    if(checkValidation(givenUrl)){
-        const urlData = [{
-            "original_URL":givenUrl,
-            "new_URL": shortid.generate()
-        }];
-        fs.writeFile("./DataBase.json",JSON.stringify(urlData),'utf8',(err)=>{
-            if (err) throw err;
-        })
-        res.send(urlData);
-    }
-    res.send({"error":"Invalid URL"});
+    if(!checkValidationOfUrl(givenUrl)){
+        res.send({"error":"Invalid URL"});
+    } else{
+        checkValidationOfHost(givenUrl).then(()=>{
+            const urlData = [{
+                "original_URL":givenUrl,
+                "new_URL": shortid.generate()
+            }];
+            fs.writeFile("./DataBase.json",JSON.stringify(urlData),'utf8',(err)=>{
+                if (err) throw err;
+            })
+            res.send(urlData);
     
+        }).catch(()=>{
+            res.send({"error":"Invalid Hostname"});
+        })
+    }
 })
 
 router.get("/:shorturl", (req, res)=>{
@@ -33,7 +39,7 @@ router.get("/:shorturl", (req, res)=>{
             catch(error){
                 if(err){
                     res.status(500);
-                    res.send({"msg":"we have a problem in our server please try again later"});
+                    res.send({"msg":"We have a problem in our server please try again later"});
                 }
                 res.status(404);
                 res.send({"error":"No short URL found for the given input"});
@@ -41,14 +47,25 @@ router.get("/:shorturl", (req, res)=>{
         })
 })
 
-function checkValidation(url){
-        try {
-          new URL(url);
-        } catch (e) {
-          console.error(e);
-          return false;
-        }
-        return true;
+function checkValidationOfHost(url){
+    const hostName = new URL(url).hostname;
+    return new Promise((resolve,reject)=>{
+        dns.lookup(hostName,(err)=>{
+            if(err){
+                reject()
+            }
+            resolve()
+        })
+    })
+}
+
+function checkValidationOfUrl(url){
+    try {
+        new URL(url);
+      } catch (e) {
+        return false;
+      }
+      return true;
 }
 
 module.exports = router;

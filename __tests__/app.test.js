a9d0c8ef7ba85a3a39d22a817a8e8e6fa0558735
OmerBotbol <omerbot2@gmail.com>
2021-03-04 11:30:26 +0200

    add GET test for unknown shorturl

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,56 +17,62 @@ const mockURLs = [
         url:"https://noubpiubpibpub.com"
     }
 ]
-describe("POST entry points: ",()=>{
-    // it("should create new url when adding url that doesn't exist yet", async ()=>{
-        
-    //     jest.spyOn(shortid, 'generate').mockReturnValue("abcdef");
-        
-    //     const response = await request(app).post("/api/shorturl/new").send(mockURLs[1]);
-        
-    //     expect(response.status).toBe(200)
-    //     expect(response.body.original_URL).toEqual(mockURLs[1].url);
-    //     expect(response.body.new_URL).toEqual("abcdef");
-
-    //     jest.spyOn(shortid, 'generate').mockRestore();
-    // })
-
-    it("should not change the new url when exist url is enteres", async ()=>{
-
-        const firstResponse = await request(app).post("/api/shorturl/new").send(mockURLs[0]);
-        const secondResponse = await request(app).post("/api/shorturl/new").send(mockURLs[0]);
-        expect(firstResponse.status).toBe(200);
-        expect(firstResponse.body).toEqual(secondResponse.body);
+describe("urlshort router:",()=>{
+    describe("POST entry points: ",()=>{
+        // it("should create new url when adding url that doesn't exist yet", async ()=>{
+            
+        //     jest.spyOn(shortid, 'generate').mockReturnValue("abcdef");
+            
+        //     const response = await request(app).post("/api/shorturl/new").send(mockURLs[1]);
+            
+        //     expect(response.status).toBe(200)
+        //     expect(response.body.original_URL).toEqual(mockURLs[1].url);
+        //     expect(response.body.new_URL).toEqual("abcdef");
+    
+        //     jest.spyOn(shortid, 'generate').mockRestore();
+        // })
+    
+        it("should not change the new url when exist url is enteres", async ()=>{
+    
+            const firstResponse = await request(app).post("/api/shorturl/new").send(mockURLs[0]);
+            const secondResponse = await request(app).post("/api/shorturl/new").send(mockURLs[0]);
+            expect(firstResponse.status).toBe(200);
+            expect(firstResponse.body).toEqual(secondResponse.body);
+        })
+    
+        it('should send "invalid url" error when invalid url is sent', async()=>{
+    
+            const response = await request(app).post("/api/shorturl/new").send(mockURLs[2]);
+            expect(response.status).toBe(200);
+            expect(response.body.error).toEqual("Invalid URL");
+    
+        })
+        it('should send "invalid hostName" error when invalid hostName is sent', async()=>{
+    
+            const response = await request(app).post("/api/shorturl/new").send(mockURLs[3]);
+            expect(response.status).toBe(200);
+            expect(response.body.error).toEqual("Invalid Hostname");
+    
+        })
     })
-
-    it('should send "invalid url" error when invalid url is sent', async()=>{
-
-        const response = await request(app).post("/api/shorturl/new").send(mockURLs[2]);
-        expect(response.status).toBe(200);
-        expect(response.body.error).toEqual("Invalid URL");
-
+    
+    describe("GET entry points: ",()=>{
+        it("should redirect to original url based on the short url and update redirectCount ",async()=>{
+            const postRequest = await request(app).post("/api/shorturl/new").send(mockURLs[0]);
+            let databaseArr = await dataBase.read();
+            const redirectCount = databaseArr.find(url=> url.original_URL === mockURLs[0].url).redirectCount;
+            const response = await request(app).get(`/api/shorturl/${postRequest.body.new_URL}`);
+            databaseArr = await dataBase.read();
+            const newRedirectCount = databaseArr.find(url=> url.original_URL === mockURLs[0].url).redirectCount;
+            expect(response.status).toBe(302);
+            expect(response.header.location).toEqual(mockURLs[0].url);
+            expect(newRedirectCount).toBe(redirectCount + 1);
+        })
+    
+        it(`should send "No short URL found for the given input" when short URL doesn't exists` ,async()=>{
+            const response = await request(app).get(`/api/shorturl/${shortid.generate()}`);
+            expect(response.status).toBe(404);
+            expect(response.body.error).toEqual("No short URL found for the given input");
+        })
     })
-    it('should send "invalid hostName" error when invalid hostName is sent', async()=>{
-
-        const response = await request(app).post("/api/shorturl/new").send(mockURLs[3]);
-        expect(response.status).toBe(200);
-        expect(response.body.error).toEqual("Invalid Hostname");
-
-    })
-})
-
-describe("GET entry points: ",()=>{
-    it("should redirect to original url based on the short url and update redirectCount ",async()=>{
-        const postRequest = await request(app).post("/api/shorturl/new").send(mockURLs[0]);
-        let databaseArr = await dataBase.read();
-        const redirectCount = databaseArr.find(url=> url.original_URL === mockURLs[0].url).redirectCount;
-        const response = await request(app).get(`/api/shorturl/${postRequest.body.new_URL}`);
-        databaseArr = await dataBase.read();
-        const newRedirectCount = databaseArr.find(url=> url.original_URL === mockURLs[0].url).redirectCount;
-        expect(response.status).toBe(302);
-        expect(response.header.location).toEqual(mockURLs[0].url);
-        expect(newRedirectCount).toBe(redirectCount + 1);
-    })
-
-
 })
